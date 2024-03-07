f9b3e5c83f2b0b990c7933859478cdfbb9eaa9c3
raven4ever <wrtv23@gmail.com>
2024-03-07 18:49:45 +0200

    refactor stringToInt

--- config/config.go
+++ config/config.go
@@ -19,16 +19,16 @@ func loadEnvirontmentConfig() *Config {
 	return &Config{
 		RedisConfig: RedisConfig{
 			Host:     getEnvVariable("REDIS_HOST", "127.0.0.1"),
-			Port:     stringToPort(getEnvVariable("REDIS_PORT", "6379")),
+			Port:     stringToInt(getEnvVariable("REDIS_PORT", "6379")),
 			Username: getEnvVariable("REDIS_USERNAME", ""),
 			Password: getEnvVariable("REDIS_PASSWORD", ""),
-			DB:       stringToPort(getEnvVariable("REDIS_DB", "0")),
-			TTL:      time.Duration(stringToPort(getEnvVariable("REDIS_TTL", "5"))) * time.Minute,
+			DB:       stringToInt(getEnvVariable("REDIS_DB", "0")),
+			TTL:      time.Duration(stringToInt(getEnvVariable("REDIS_TTL", "5"))) * time.Minute,
 		},
 		ServerConfig: ServerConfig{
-			Port: stringToPort(getEnvVariable("PORT", "8080")),
+			Port: stringToInt(getEnvVariable("PORT", "8080")),
 		},
-		ShortUrlLength: stringToPort(getEnvVariable("SHORT_URL_LENGTH", "7")),
+		ShortUrlLength: stringToInt(getEnvVariable("SHORT_URL_LENGTH", "7")),
 	}
 }
 
@@ -39,10 +39,10 @@ func getEnvVariable(key, defaultValue string) string {
 	return defaultValue
 }
 
-func stringToPort(port string) int {
-	intPort, err := strconv.Atoi(port)
+func stringToInt(intValue string) int {
+	intPort, err := strconv.Atoi(intValue)
 	if err != nil {
-		log.Fatalf("%s is not a valid port number", port)
+		log.Fatalf("%s is not a valid int number", intValue)
 	}
 	return intPort
 }
